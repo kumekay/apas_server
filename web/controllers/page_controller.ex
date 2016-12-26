7ea4d73028b58e81ffe2a89e86a9fc1c4d0aacc2
Sergei Silnov <po@kumekay.com>
2016-12-26 09:23:06 -0800

    fix layout

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -5,9 +5,9 @@ defmodule ApasServer.PageController do
 
   def index(conn, _params) do
 
-    pourings = from(p in ApasServer.Pouring, select: [p.volume, p.inserted_at]) |> Repo.all
+    pourings = from(p in ApasServer.Pouring, order_by: [desc: p.inserted_at], select: [p.volume, p.inserted_at]) |> Repo.all
 
-    bottle_changes = from(bc in ApasServer.BottleChange, select:  bc.inserted_at) |> Repo.all
+    bottle_changes = from(bc in ApasServer.BottleChange, order_by: [desc: bc.inserted_at], select:  bc.inserted_at) |> Repo.all
 
     last_bottle_change = from(bc in ApasServer.BottleChange, order_by: [desc: bc.inserted_at], select: bc.inserted_at, limit: 1) |> ApasServer.Repo.one
 
